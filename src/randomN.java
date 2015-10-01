dc052a45dc6eede2e346b1626cfec209f687e208
SAC-CS112-REYES-JESUS <kingsirluke@gmail.com>
2015-09-30 19:51:56 -0700

    lab1

---
+++ src/randomN.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class randomN 
+{
+
+	
+	public static void main(String[] args)
+	
+	{
+		Scanner input = new Scanner ( System.in);
+		
+		int computerNum;
+		int userNum;
+		
+		computerNum = 1 + (int) ( Math.random() * 10 );
+		
+
+		
+	System.out.println("Guess the number between 1-10?");
+	userNum=input.nextInt();
+	
+			
+	if(userNum==computerNum)
+		{
+		System.out.println("You guess the correct number ;]");
+		}
+		
+		else if(userNum>computerNum)
+		{
+		System.out.println("You number is greater than the number ;{");
+		}
+		else if(userNum<computerNum)
+		{
+		System.out.println("You number is to small ;P");
+		}
+	
+	System.out.printf("The random number is: %d",computerNum);
+	
+		
+	}
+	
+	
+	
+	}
+
+
+
